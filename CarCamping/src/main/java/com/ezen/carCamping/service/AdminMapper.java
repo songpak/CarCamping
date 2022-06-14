4c80bd17002c25dfb15608ba80e3d964a97ab850
xongsama <thdwodud1996@gmail.com>
2022-06-14 21:00:32 +0900

    [update] 테이블로 변경

--- CarCamping/src/main/java/com/ezen/carCamping/service/AdminMapper.java
+++ CarCamping/src/main/java/com/ezen/carCamping/service/AdminMapper.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ezen.carCamping.dto.RegionDTO;
 
+@Service
 public class AdminMapper {
 
 	@Autowired
 	private SqlSession sqlSession;
 	
-	private List<RegionDTO> listRegion(){
-		List<RegionDTO> list = sqlSession.selectList("listRegion");
+	public List<RegionDTO> adminListRegion(){
+		List<RegionDTO> list = sqlSession.selectList("adminListRegion");
 		return list;
 	}
 }
